19174e35c3729d365cc2459dd5ba763c4e985415
Sara Steele <sara.steele14@gmail.com>
2016-06-13 21:15:47 -0400

    about to change the escape characters in re.sub

--- get_song_lyrics.py
+++ get_song_lyrics.py
@@ -1,11 +1,13 @@
 import requests
 import urllib
-from urllib import quote_plus, quote
+import json
+import re
+import pdb 
+
+from urllib import quote_plus
 from urlparse import urljoin
 from bs4 import BeautifulSoup
-import re
 
-import pdb 
 
 def get_song_lyrics(song_title=None,artist=None):
 
@@ -28,7 +30,7 @@ def get_song_lyrics(song_title=None,artist=None):
         url2 = "https://www.musixmatch.com/lyrics/" + artist + \
                            "/" + song_title
 
-        print url2
+        #print url2
 
         title = originalArtist + ": " + originalTitle
         
@@ -39,69 +41,69 @@ def get_song_lyrics(song_title=None,artist=None):
 
         bInstr = re.findall("([A-Z]\w+)",str(checkInstrumental))
 
+        try:
+            if re.findall("Instrumental",bInstr[0]):
+                print "that's an instrumental track!"
+                lyrics="Instrumental"
+                return lyrics, title, url2
 
-        if re.findall("Instrumental",bInstr[0]):
-            print "that's an instrumental track!"
-            lyrics=None
-            return lyrics, title
-
-        scripts = soup.find_all("script")
-
-        for script in scripts:
-            tmp = script.text.split("__mxmState = ")
-            if len(tmp)>1:
-                data = tmp[1][:-1]
-                jsdata = json.loads(data)
-                break
-
-
+        except IndexError:
         
+            scripts = soup.find_all("script")
+
+            for script in scripts:
+                tmp = script.text.split("__mxmState = ")
+                if len(tmp)>1:
+                    data = tmp[1][:-1]
+                    jsdata = json.loads(data)
+                    break
+
+            try:
+                lyrics = jsdata['page']['lyrics']['lyrics']['body']
+
+
+                # lyrics = re.findall("body\":\"(.*?)\"",response)
+                # lyrics = lyrics[0]
+
+                title = originalArtist + ": " + originalTitle
+                print "found lyrics on musixmatch!"
+                return lyrics, title, url2
+            except:
+
+                print "couldn't find on musixmatch"
+                pass
+
+        print "searching chartlyrics"
+        BASE_URL = "http://www.chartlyrics.com/"
+        #queryString = "track.search?q_track=" #chartlyrics would return a soap object if i properly queried it
+        searchURL = BASE_URL + "search.aspx?q=" + song_title
+        
+        fullurl = quote_plus(searchURL, safe="%/:=&?~#!+$,;'@()*[]")
+        #fullurl = quote(searchURL, safe="%/:=&?~#!+$,;'@()*[]")
+
+        response = requests.get(fullurl)
+        soup = BeautifulSoup(response.text,'lxml')
+
+        tmp = soup.find_all('a')
+        song_link = tmp[5];
+
+        # i could also do soup.find('table')
+
+        link = urljoin(BASE_URL, song_link['href'])
+        response = requests.get(link);
+
+        soup = BeautifulSoup(response.text,'lxml')
+        lyrics_box = soup.find('p')#.text.strip()
 
         try:
-            lyrics = jsdata['page']['lyrics']['lyrics']['body']
+            lyrics = lyrics_box.text.strip()
 
+            title = soup.find('head').find('title').text
 
-            # lyrics = re.findall("body\":\"(.*?)\"",response)
-            # lyrics = lyrics[0]
+            # re.findall("u\'(.*)\\t",
 
-            title = originalArtist + ": " + originalTitle
-            print "found lyrics on musixmatch!"
-            return lyrics, title
         except:
-            print "couldn't find on musixmatch"
-            pass
-
-    print "searching chartlyrics"
-    BASE_URL = "http://www.chartlyrics.com/"
-    #queryString = "track.search?q_track=" #chartlyrics would return a soap object if i properly queried it
-    searchURL = BASE_URL + "search.aspx?q=" + song_title
-    
-    fullurl = quote_plus(searchURL, safe="%/:=&?~#!+$,;'@()*[]")
-    #fullurl = quote(searchURL, safe="%/:=&?~#!+$,;'@()*[]")
-
-    response = requests.get(fullurl)
-    soup = BeautifulSoup(response.text,'lxml')
-
-    tmp = soup.find_all('a')
-    song_link = tmp[5];
-
-    # i could also do soup.find('table')
-
-    link = urljoin(BASE_URL, song_link['href'])
-    response = requests.get(link);
-
-    soup = BeautifulSoup(response.text,'lxml')
-    lyrics_box = soup.find('p')#.text.strip()
-
-    try:
-        lyrics = lyrics_box.text.strip()
-
-        title = soup.find('head').find('title').text
-
-        # re.findall("u\'(.*)\\t",
-
-    except:
-        
-            print "Couldn't find the lyrics. Try another song."
-            return
-    return lyrics, title
+            
+                print "Couldn't find the lyrics. Try another song."
+                return
+        return lyrics, title, link
